1f17952c53a0cd6a020de83b5d8388129d801b9b
randoelicor <Jose_Leonard_Rico@manulife.ca>
2022-03-25 14:17:35 +0800

    Added Bookings component

---
+++ src/components/Bookings/BookingsGrid.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BookingsGrid = (props) => {
+  const { week, bookable, booking, setBooking } = props;
+
+  return (
+    <div className="bookings-grid placeholder">
+      <h3>Bookings Grid</h3>
+      <p>{bookable?.title}</p>
+      <p>{week.date.toISOString()}</p>
+    </div>
+  )
+};
+
+export default BookingsGrid;
